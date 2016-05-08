39ca01388032b9c637e6ad7c82a21c16ea51e2ce
Konstantin Schubert <schubert.konstantin@gmail.com>
2016-05-08 16:06:08 +0200

    working on gui

---
+++ dataframe_managers.py
@@ -0,0 +1,105 @@
+import pandas
+import root_pandas
+
+class DataFrameManager:
+    """
+        Use this class to get the DataFrame you need.
+
+        This is an sub implementation to define the public interface.
+
+    """
+    def __init__(self, filename):
+        #stub
+        pass
+
+    def get_DataFrame(self, columns,query):
+        #stub
+        pass
+    def get_all_columns():
+        #stub
+        pass
+
+
+class DataFrameManagerROOT(DataFrameManager):
+    """
+        Use this class if you are interfacing a root file.
+
+        This class manages the loading and filtering of the data. 
+        It can decides which columns it loads from the root file and 
+        how much of it is being stored.
+    """
+    max_priority = 10
+    def __init__(self, filename, treename = None):
+        self.filename = filename
+        self.treename = treename
+        # The raw, unfiltered DataFrame. 
+        self._raw_dataset = pandas.DataFrame()
+        # a list of Column objects
+        self.columns_needed = []
+
+
+    def _prune_columns_needed(self, columns_needed):
+        """
+            Prevents the list of needed columns from growing to big.
+
+            Can be more aggressive if needed.
+        """
+
+        columns_needed.sort(key=lambda x : x.priority)
+        # double iteration should not be a big problem due to shortness of list
+        pruned = []
+        for index in range(0,len(columns_needed)):
+            # if there is no later entry with the same name (and higher priority)
+            if columns_needed[index].name not in [x.name for x in columns_needed[index+1:] ]:
+                pruned.append(columns_needed[index])
+        columns_needed = pruned
+
+        # we could now drop columns with very low priority
+        # but we must be careful to keep the columns with the highest priority
+
+
+    def get_DataFrame(self, columns, query=None):
+        """
+        Returns the desired DataFrame.
+
+        columns : list of strings
+            List of columns that are needed. Must be frugal. All columns that
+            are used in the query must be listed here.
+        query : string
+            A string to pass to the pandas.DataFrame.query method.
+        """
+        for column in columns:
+            self.columns_needed.append(Column(column, DataFrameManagerROOT.max_priority))
+        self._prune_columns_needed(self.columns_needed)
+
+        # If we run in memory troubles, we could here
+        # remove columns form the raw_dataset which are not longer needed
+
+        columns_to_load = [ x.name for x in self.columns_needed if x.name not in self._raw_dataset.columns.values ]
+        loaded = root_pandas.read_root(self.filename, self.treename, columns=columns_to_load)
+        assert( len(loaded.index) == len(self._raw_dataset.index) or len(self._raw_dataset.index) == 0 )
+        self._raw_dataset = pandas.concat([self._raw_dataset, loaded] , axis=1)
+
+        # reduce column priority
+        for column in self.columns_needed:
+            column.priority -= 1
+        if query is None:
+            return self._raw_dataset
+        else:
+            return self._raw_dataset.query(query)
+
+
+    def get_all_columns(self):
+        ignore = ["*COV*"] # \todo: Ignore all array-branches, don't just use a heuristic
+        df = root_pandas.read_root(self.filename, self.treename, chunksize=1).next()
+        return df.columns.values
+
+
+
+
+class Column:
+    def __init__(self,name, priority):
+        self.name = name
+        self.priority = priority
+
+
---
+++ gui.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+
+from __future__ import unicode_literals
+import sys
+import os
+import random
+from PyQt4 import QtGui, QtCore
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
+
+class MyMplCanvas(FigureCanvasQTAgg):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvasQTAgg.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvasQTAgg.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvasQTAgg.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class PlotCanvas(MyMplCanvas):
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(10000)
+
+    def compute_initial_figure(self):
+        pass
+
+    def update_figure(self):
+        # Build a list of 4 random integers between 0 and 10 (both inclusive)
+        l = [random.randint(0, 10) for i in range(4)]
+
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        self.draw()
+
+
+class ApplicationWindow(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("Pandas Gui")
+
+        self.file_menu = QtGui.QMenu('&File', self)
+        self.file_menu.addAction('&Open', self.openFile, QtCore.Qt.CTRL + QtCore.Qt.Key_O)
+        self.file_menu.addAction('&Quit', self.fileQuit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        # self.help_menu = QtGui.QMenu('&Help', self)
+        # self.menuBar().addSeparator()
+        # self.menuBar().addMenu(self.help_menu)
+
+        # self.help_menu.addAction('&About', self.about)
+
+
+
+        self.main_widget = QtGui.QWidget(self)
+
+        # sidebar
+        queryLabel = QtGui.QLabel("Search:");
+        queryEdit = QtGui.QLineEdit();
+        filtered_available_vars = QtGui.QTableView(); # better use list view here
+
+        queryLayout = QtGui.QHBoxLayout();
+        queryLayout.addWidget(queryLabel);
+        queryLayout.addWidget(queryEdit);
+
+        sidebar = QtGui.QVBoxLayout();
+        sidebar.addLayout(queryLayout);
+        sidebar.addWidget(filtered_available_vars);
+
+
+        # main view
+        bigview = QtGui.QVBoxLayout();
+        variables_to_plot = QtGui.QLineEdit(text="here go variables to plot like tags on SO", parent=self.main_widget)
+        cutstring = QtGui.QLineEdit(text="here goes cutstring as text", parent=self.main_widget)
+        plot_canvas = PlotCanvas(self.main_widget, width=5, height=4, dpi=100)
+        bigview.addWidget(variables_to_plot)
+        bigview.addWidget(cutstring)
+        bigview.addWidget(plot_canvas)
+        
+        
+        # putting it all together
+        main_layout = QtGui.QHBoxLayout(self.main_widget)
+        main_layout.addLayout(sidebar);
+        main_layout.addLayout(bigview)
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("By Konstantin Schubert!", 2000)
+
+    def openFile(self):
+        pass
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+
+
+qApp = QtGui.QApplication(sys.argv)
+
+aw = ApplicationWindow()
+aw.setWindowTitle("%s" % progname)
+aw.show()
+sys.exit(qApp.exec_())
+#qApp.exec_()
