96e9e923a2da0c3f529a80f2b59c9fbd09cb1519
KonstantinSchubert <schubert.konstantin@gmail.com>
2016-05-09 21:56:55 +0200

    added setup.py (not yet tested)

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup
+import sys
+import os
+home=os.path.expanduser('~')
+
+setup(
+    name = 'data_gui',
+    version = '0.0.1',
+    description = 'Dataset viewer for pandas, especially large ROOT files',
+    # url='',
+    # license='GPL v3',
+    author = 'Konstantin Schubert',
+    author_email = 'konstantin@schubert.fr',
+    packages = ['data_gui'],
+    install_requires=['root_pandas',
+                      'matplotlib',
+                      'pandas',
+                      'pyqt4',
+                      'future'],
+    entry_points = { 'gui_scripts': [
+                     'datagui = data_gui.gui:main']},
+    classifiers = ['Operating System :: OS Independent',
+    		'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Topic :: Software Development :: User Interfaces',
+            # 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Intended Audience :: Science/Research'],
+    keywords = ['ROOT', 'root_pandas', 'table', 'pandas', 'data analysis'],
+)
